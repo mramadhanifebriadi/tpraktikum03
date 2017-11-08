862cd2fa5d7d4cef9fb299a66327d27e0e7da115
Muhammad Ramadhani Febriadi <32356499+mramadhanifebriadi@users.noreply.github.com>
2017-11-08 11:33:12 +0700

    membuat output dengan percabanagn

--- soalnomor5.cpp
+++ soalnomor5.cpp
@@ -10,5 +10,12 @@ int main() {
 cin >>t;
 cout << "Silahkan masukkan berat badan(kg) : " ;
 cin >> b;
+if (b<t/2.5)
+cout << "  Underweight" << endl;
+else if (t/2.3 < b)
+cout << "  Overweight" << endl;
+else if (t/2.5 <= b <= t/2.3)
+cout << "  Normal" << endl;
+
 }
 
