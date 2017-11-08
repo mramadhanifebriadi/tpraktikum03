aee5701db59858d19a7fb0f685b434d992d0b13f
Muhammad Ramadhani Febriadi <32356499+mramadhanifebriadi@users.noreply.github.com>
2017-11-08 11:11:38 +0700

    memasukan program percabangan
    dengan ketentuan jika user input huruf p maka program akan menghitung persegi dan jika user input huruf s maka program akan menghitung luas segitiga

--- soalnomor2.cpp
+++ soalnomor2.cpp
@@ -9,4 +9,26 @@ int main() {
 	char inisial;
 	cout<<"selamat datang di program perhitungan bangun datar \n"
 		<<"Input inisial bangun datar apa yang ingin anda hitung:"; cin >> inisial;
+		
+		if (inisial=='p'||inisial=='P')
+		{
+			cout << "Anda menginputkan inisial untuk menghitung persegi \n"
+				<<"input tinggi:"; cin>>t; cout <<"cm \n"
+				<<"input lebar:"; cin>>l; cout <<"cm"<<endl;
+				cout <<"luas persegi:"; 
+				cout<<t*l;
+				cout<<"cm"<<endl;
+		}
+		else if (inisial=='s'||inisial=='S')
+		{
+			cout << "Anda menginputkan inisial untuk menghitung segitiga \n"
+				<<"input tinggi:"; cin>>t; cout <<"cm \n"
+				<<"input alas:"; cin>>a; cout <<"cm"<<endl;
+				cout <<"luas segitiga:"; 
+				cout<<0.5*a*t;
+				cout<<"cm"<<endl;
+		}
+		else
+		cout <<"inisial yg anda inputkan tidak tersedia \n"
+			<<"ssilahkan input 'p' untuk persegi dan 's' untuk segitiga"; 
 }
