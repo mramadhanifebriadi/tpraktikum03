9ab7cd66dd93186137a903004ccfd6827fba6a9c
Muhammad Ramadhani Febriadi <32356499+mramadhanifebriadi@users.noreply.github.com>
2017-11-08 11:29:57 +0700

    AKHIR PROGRAM MEMBUAT INPUT DAN OUTPUT NYA

--- soalnomor4.cpp
+++ soalnomor4.cpp
@@ -3,3 +3,17 @@ using namespace std;
 
 int main(){
 	int angka;
+	cout<<"Input angka : ";cin>>angka;
+	if (angka < 0)
+	{
+		cout<<"bilangan yang anda input negatif"<<endl;
+	}
+    else if (angka>0)
+	{
+		cout<<"bilangan yang anda input positif"<<endl;
+	}
+    else
+	
+		cout<<"bilangan yang anda input nol "<<endl;
+
+}
